7ca5e84e7e3021c3843880e58487b484be41f8da
LAMEfreak <leeweirong@outlook.com>
2024-05-26 09:50:19 +0800

    Install Tailwind & TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  const test = (a: number, b: number): number => a + b;
+
+  return (
+    <>
+      <h1 className="text-4xl">Video Calling App</h1>
+    </>
+  );
+}
+
+export default App;
