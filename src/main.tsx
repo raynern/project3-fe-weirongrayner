f2723e2571542a81515e1858d578e57ecbee0b88
LAMEfreak <leeweirong@outlook.com>
2024-05-28 12:11:03 +0800

    Add Auth0 SDK and configure Auth0Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-wnqp04xircjjnguk.us.auth0.com"
+      clientId="C6fevYOkunrHlgHIXg31FGWnU7zycUPI"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173/",
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
