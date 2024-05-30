f4986c49464c658694b68137f833a651e1ae5059
LAMEfreak <leeweirong@outlook.com>
2024-05-30 18:40:46 +0800

    Implement Auth0 redirect on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
+import LogoutButton from "../components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Dashboard = () => {
+  const { isAuthenticated, user, getAccessTokenSilently, loginWithRedirect } =
+    useAuth0();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  });
+
   return (
-    <div>Dashboard</div>
-  )
-}
-export default Dashboard
+    isAuthenticated && (
+      <div className="p-8 flex flex-col items-center align-middle">
+        <p className="text-2xl mb-4">Dashboard</p>
+        <LogoutButton />
+      </div>
+    )
+  );
+};
+export default Dashboard;
